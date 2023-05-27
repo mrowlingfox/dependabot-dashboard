3dc8f69999b096b5fad44b36ad3125ed7e74dfcf
Mitchell Rowling <mitchell.rowling@foxtel.com.au>
2023-05-28 00:26:32 +1000

    Query all the data then write into DB

--- fetch-alerts.py
+++ fetch-alerts.py
@@ -12,18 +12,60 @@ db_host = os.environ["DB_HOST"]
 host = os.environ["GH_HOST"]
 gh_token = os.environ["GH_TOKEN"]
 
+TABLE_NAME = "dependabot_alerts_mrowling"
+
 connection = psycopg2.connect(user=db_user,
                                       password=db_password,
                                       host=db_host,
                                       port="5432",
                                       database="dependabot")
 
+
+# create file if not exists, suuport creating directories too. Write empty json object to contents
+def create_file_if_not_exists(file_path):
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as file:
+            json.dump({}, file)
+
+
+# Function to use as a decorator to memoize and cache on disk. Supports multiple arguments.\
+# Create the cache file if it doesn't exist
+# Use pickle to serialize the result
+def memoize_and_cache_on_disk(cache_file):
+    create_file_if_not_exists(cache_file)
+    def memoize_and_cache_on_disk_decorator(func):
+        def memoize_and_cache_on_disk_wrapper(*args, **kwargs):
+            try:
+                with open(cache_file, 'rb') as cache:
+                    cache_dict = json.load(cache)
+            except FileNotFoundError:
+                cache_dict = {}
+            key = json.dumps({
+                "args": args,
+                "kwargs": kwargs
+            })
+            if key in cache_dict.keys():
+                print("Cache hit for: ", key)
+                return cache_dict[key]
+            else:
+                print("Cache miss for: ", key)
+                result = func(*args, **kwargs)
+                cache_dict[key] = result
+                with open(cache_file, 'w') as cache:
+                    json.dump(cache_dict, cache)
+                return result
+        return memoize_and_cache_on_disk_wrapper
+    return memoize_and_cache_on_disk_decorator
+
 def initialize_db():
+    print("Init DB")
     try:
         cursor = connection.cursor()
 
-        postgres_create_table = """ 
-            create table if not exists dependabot_alerts(
+        postgres_create_table = f""" 
+            create table if not exists {TABLE_NAME} (
                 snapshot timestamp,
                 gh_repo varchar(100),
                 gh_org varchar(30),
@@ -50,52 +92,92 @@ def initialize_db():
     except (Exception, psycopg2.Error) as error:
         print("Failed to initialize dependabot_alerts table: ", error)
 
-def insert_into_db(alert, gh_org, gh_repo):
+def parse_alert(alert, gh_org, gh_repo):
+    snapshot_date = date.today()
+    snapshot_timestamp = datetime.strptime(
+            str(snapshot_date), "%Y-%m-%d").strftime("%Y-%m-%dT00:00:00Z")
+    repo = gh_org + "/" + gh_repo
+    org = gh_org
+    created_at = alert.get("createdAt")
+    fixed_at = alert.get("fixedAt")
+
+    alert_number = alert.get("number")
+    state = alert.get("state")
+    dismissed_at = alert.get("dismissedAt")
+    dismiss_reason = alert.get("dismissReason")
+    dismisser = None
+    if alert.get("dismisser") is not None:
+        dismisser = alert.get("dismisser").get("login")
+
+    vuln_ghsa_id = alert.get("securityVulnerability", {}).get("advisory").get("ghsaId")
+    vuln_severity = alert.get("securityVulnerability", {}).get("severity")
+    vuln_summary = alert.get("securityVulnerability", {}).get("advisory").get("summary")
+    vuln_package = alert.get("securityVulnerability", {}).get("package").get("name")
+
+    fix_pr_number = None
+    fix_pr_title = None
+    fix_merged_at = None
+
+    if alert.get("dependabotUpdate") is not None:
+        if alert.get("dependabotUpdate").get("pullRequest") is not None:
+            fix_pr_number = alert.get("dependabotUpdate").get("pullRequest").get("number")
+            fix_pr_title = alert.get("dependabotUpdate").get("pullRequest").get("title")
+            fix_merged_at = alert.get("dependabotUpdate").get("pullRequest").get("mergedAt")
+
+    if state == "FIXED" or state == "DISMISSED":
+        tmp_now = datetime.strptime(
+            str(snapshot_date), "%Y-%m-%d")
+        if fixed_at is not None:
+            tmp_fixed_at = datetime.strptime(fixed_at, "%Y-%m-%dT%H:%M:%SZ")
+            if tmp_fixed_at >= tmp_now:
+                state = "OPEN"
+        if dismissed_at is not None:
+            tmp_dismissed_at = datetime.strptime(dismissed_at, "%Y-%m-%dT%H:%M:%SZ")
+            if tmp_dismissed_at >= tmp_now:
+                state = "OPEN"
+    
+    db_values = (snapshot_timestamp, repo, org, created_at, fixed_at, alert_number, state, dismissed_at, dismiss_reason,
+                dismisser, vuln_ghsa_id, vuln_severity, vuln_summary, vuln_package, fix_pr_number, fix_pr_title, fix_merged_at)
+    
+    return db_values
+
+# This function is used to insert multiple records into the dependabot_alerts table
+# it will accept a list of tuples, each tuple is a record to be inserted
+# the tuples must be in the same order as the columns in the table
+# The columns are:
+# snapshot, gh_repo, gh_org, created_at, fixed_at, alert_number, state, dismissed_at, dismiss_reason, dismisser, vuln_ghsa_id, vuln_severity, vuln_summary, vuln_package, fix_pr_number, fix_pr_title, fix_merged_at
+def bulk_insert_into_db(db_values):
     try:
-        snapshot_date = date.today()
-        snapshot_timestamp = datetime.strptime(
-                str(snapshot_date), "%Y-%m-%d").strftime("%Y-%m-%dT00:00:00Z")
-        repo = gh_org + "/" + gh_repo
-        org = gh_org
-        created_at = alert.get("createdAt")
-        fixed_at = alert.get("fixedAt")
+        insert_query = f"""
+            insert into {TABLE_NAME} (
+                snapshot,
+                gh_repo,
+                gh_org,
+                created_at,
+                fixed_at,
+                alert_number,
+                state,
+                dismissed_at,
+                dismiss_reason,
+                dismisser,
+                vuln_ghsa_id,
+                vuln_severity,
+                vuln_summary,
+                vuln_package,
+                fix_pr_number,
+                fix_pr_title,
+                fix_merged_at
+            ) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+        """
 
-        alert_number = alert.get("number")
-        state = alert.get("state")
-        dismissed_at = alert.get("dismissedAt")
-        dismiss_reason = alert.get("dismissReason")
-        dismisser = None
+        cursor = connection.cursor()
+        cursor.executemany(insert_query, db_values)
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert records into dependabot_alerts table: ", error)
 
-        if alert.get("dismisser") is not None:
-            dismisser = alert.get("dismisser").get("login")
-        
-        vuln_ghsa_id = alert.get("securityVulnerability").get("advisory").get("ghsaId")
-        vuln_severity = alert.get("securityVulnerability").get("severity")
-        vuln_summary = alert.get("securityVulnerability").get("advisory").get("summary")
-        vuln_package = alert.get("securityVulnerability").get("package").get("name")
-
-        fix_pr_number = None
-        fix_pr_title = None
-        fix_merged_at = None
-
-        if alert.get("dependabotUpdate") is not None:
-            if alert.get("dependabotUpdate").get("pullRequest") is not None:
-                fix_pr_number = alert.get("dependabotUpdate").get("pullRequest").get("number")
-                fix_pr_title = alert.get("dependabotUpdate").get("pullRequest").get("title")
-                fix_merged_at = alert.get("dependabotUpdate").get("pullRequest").get("mergedAt")
-
-        if state == "FIXED" or state == "DISMISSED":
-            tmp_now = datetime.strptime(
-                str(snapshot_date), "%Y-%m-%d")
-            if fixed_at is not None:
-                tmp_fixed_at = datetime.strptime(fixed_at, "%Y-%m-%dT%H:%M:%SZ")
-                if tmp_fixed_at >= tmp_now:
-                    state = "OPEN"
-            if dismissed_at is not None:
-                tmp_dismissed_at = datetime.strptime(dismissed_at, "%Y-%m-%dT%H:%M:%SZ")
-                if tmp_dismissed_at >= tmp_now:
-                    state = "OPEN"
-
+def insert_into_db(db_values):
+    try:
         insert_query = """
             insert into dependabot_alerts (
                 snapshot,
@@ -118,31 +200,34 @@ def insert_into_db(alert, gh_org, gh_repo):
             ) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
         """
 
-        db_values = (snapshot_timestamp, repo, org, created_at, fixed_at, alert_number, state, dismissed_at, dismiss_reason,
-                         dismisser, vuln_ghsa_id, vuln_severity, vuln_summary, vuln_package, fix_pr_number, fix_pr_title, fix_merged_at)
         cursor = connection.cursor()
         cursor.execute(insert_query, db_values)
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         print("Error when inserting into table: ", error)
-        print("DEBUG: ", str(alert))
+        print("DEBUG: ", str(db_values))
 
 
 # Get a list of the full_name of all my github repos using the github library
+@memoize_and_cache_on_disk(cache_file="cache/get_repos.cache")
 def get_repos(gh_token, gh_org):
+    print("Getting repos for: " + gh_org)
     g = Github(gh_token)
     org = g.get_organization(gh_org)
     repos = org.get_repos()
-    repo_list = []
-    for repo in repos:
-        repo_list.append(repo.full_name)
-    return repo_list
+    return [ repo.full_name for repo in repos ]
+
+def concatenate_lists(list1, list2):
+    return list1 + list2
+
+def add_to_array(arr1, arr2):
+    for i in arr2:
+        if i not in arr1:
+            arr1.append(i)
+    return arr1
 
 
-
-
-def get_alerts(host, gh_token, gh_org, gh_repo):
-    # Handle pagination
+def gql_client(host, gh_token):
     headers = {
         "Authorization": "Bearer " + gh_token,
         "Accept": "application/vnd.github.v4.idl"
@@ -151,126 +236,102 @@ def get_alerts(host, gh_token, gh_org, gh_repo):
     # Select your transport with a defined url endpoint
     transport = AIOHTTPTransport(url=host+"/graphql", headers=headers)
     # Create a GraphQL client using the defined transport
-    client = Client(transport=transport, fetch_schema_from_transport=True)
+    return Client(transport=transport, fetch_schema_from_transport=True)
 
-    # Provide a GraphQL query
-    query = gql(
-        """
-        {
-        repository(name: "%s", owner: "%s") {
-            vulnerabilityAlerts(first:100) {
-            pageInfo {
-                startCursor
-                hasNextPage
-                endCursor
-            }
-            nodes {
-                createdAt
-                fixedAt
-                number
-                dependabotUpdate {
-                pullRequest {
+def build_query(gh_repo, gh_org, first=100, after=None):
+    if after is not None:
+        q_str = f"first:{first}, after: \"{after}\""
+    else:
+        q_str = f"first:{first}"
+    query = f"""
+    {{
+        repository(name: "{gh_repo}", owner: "{gh_org}") {{
+            vulnerabilityAlerts({q_str}) {{
+                pageInfo {{
+                    startCursor
+                    hasNextPage
+                    endCursor
+                }}
+                nodes {{
+                    createdAt
+                    fixedAt
                     number
-                    title
-                    mergedAt
-                }
-                }
-                state
-                dismissedAt
-                dismisser {
-                    login
-                }
-                dismissReason
-                securityVulnerability {
-                severity
-                advisory {
-                    ghsaId
-                    summary
-                }
-                package {
-                    name
-                }
-                }
-            }
-            }
-        }
-        }
+                    dependabotUpdate {{
+                        pullRequest {{
+                            number
+                            title
+                            mergedAt
+                        }}
+                    }}
+                    state
+                    dismissedAt
+                    dismisser {{
+                        login
+                    }}
+                    dismissReason
+                    securityVulnerability {{
+                        severity
+                        advisory {{
+                            ghsaId
+                            summary
+                        }}
+                        package {{
+                            name
+                        }}
+                    }}
+                }}
+            }}
+        }}
+    }}
+    """
+    return gql(query)
 
-    """ % (gh_repo, gh_org)
-    )
+# Query will accept first and after as variables
+# After querying the first 100, it will query the next 100 using the endCursor
+def execute_gql_query_with_paging(client, gh_repo, gh_org, first, after=None):
+    query = build_query(gh_repo, gh_org, first, after)
+    result = client.execute(query)
+    alerts = result.get("repository").get("vulnerabilityAlerts").get("nodes")
 
-    while True:
-        # Execute the query on the transport
-        result = client.execute(query)
+    has_next_page = result.get("repository").get("vulnerabilityAlerts").get("pageInfo").get("hasNextPage")
+    
+    if has_next_page:
+        end_cursor = result.get("repository").get("vulnerabilityAlerts").get("pageInfo").get("endCursor")
+        next_alerts = execute_gql_query_with_paging(client, gh_repo, gh_org, first, end_cursor)
+        alerts = concatenate_lists(alerts, next_alerts)
+    
+    return alerts
 
-        for alert in result.get("repository").get("vulnerabilityAlerts").get("nodes"):
-            print(json.dumps(alert))
-            insert_into_db(alert, gh_org, gh_repo)
 
-        nextpage = result.get("repository").get(
-            "vulnerabilityAlerts").get("pageInfo").get("hasNextPage")
-        if not nextpage:
-            break
+@memoize_and_cache_on_disk(cache_file="cache/get_alerts.cache")
+def get_alerts(host, gh_token, gh_org, gh_repo):
+    # Handle pagination
+    client = gql_client(host, gh_token)
 
-        endcursor = result.get("repository").get(
-            "vulnerabilityAlerts").get("pageInfo").get("endCursor")
-        query = gql(
-            """
-        {
-        repository(name: "%s", owner: "%s") {
-            vulnerabilityAlerts(first:100, after:"%s") {
-            pageInfo {
-                startCursor
-                hasNextPage
-                endCursor
-            }
-            nodes {
-                createdAt
-                fixedAt
-                number
-                dependabotUpdate {
-                pullRequest {
-                    number
-                    title
-                    mergedAt
-                }
-                }
-                state
-                dismissedAt
-                dismisser {
-                    login
-                }
-                dismissReason
-                securityVulnerability {
-                severity
-                advisory {
-                    ghsaId
-                    summary
-                }
-                package {
-                    name
-                }
-                }
-            }
-            }
-        }
-        }
-    """ % (gh_repo, gh_org, endcursor)
-        )
+    alerts = execute_gql_query_with_paging(client, gh_repo, gh_org, 100)
 
-    return []
-
+    return alerts
 
 def main():
     initialize_db()
-    repos = get_repos(gh_token=gh_token, gh_org="fsa-streamotion")
+    gh_org = "fsa-streamotion"
+    repos = get_repos(gh_token=gh_token, gh_org=gh_org)
 
     for repo in repos:
-        print("[+] Analyzing " + repo)
+        print(f"[+] Analyzing {repo}")
         org = repo.split("/")[0]
         repo_name = repo.split("/")[1]
 
-        get_alerts(host, gh_token, org, repo_name)
+        alerts = get_alerts(host, gh_token, org, repo_name)
+        print(f"[+] Found {len(alerts)} alerts")
+        # print(f"{alerts}")
+        print(f"[+] Inserting into database")
+        database_inserts = [ parse_alert(alert, gh_org, repo_name) for alert in alerts]
+        bulk_insert_into_db(database_inserts)
+        # for alert in alerts:
+        #     data = parse_alert(alert, gh_org, repo_name)
+        #     insert_into_db(data)
+
 
 if __name__ == "__main__":
     main()
